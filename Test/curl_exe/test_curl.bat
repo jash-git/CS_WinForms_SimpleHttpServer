3b78bfc4ff37bb4c8fd1814c99dc3531a63c9fa3
jash.liao <jash.liao@gmail.com>
2022-12-08 17:42:46 +0800

    modified 202212081742

--- Test/curl_exe/test_curl.bat
+++ Test/curl_exe/test_curl.bat
@@ -5,8 +5,8 @@ chcp 65001
 
 for /l %%x in (1, 1, 1000) do (
 	echo %%x
-    curl.exe http://192.168.1.108:8001/jashliao
-    timeout 1
+    curl.exe http://192.168.1.108:8001/liaojash
+    ping 127.0.0.1 -n 1 -w 150> nul
 )
 
 pause
